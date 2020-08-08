ac363c253c15402803cc5f1ad16ca69603223417
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-08 15:21:56 +0700

    Add Template engine(handlebars)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+
 const port = 8000;
 
+// HTTP logger
 app.use(morgan("combined"));
 
+// Template engine
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
 app.get("/", (req, res) => {
-  res.send(`
-    <h1> Hello world </h1>
-  `);
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
 });
 
 app.listen(port, () => {

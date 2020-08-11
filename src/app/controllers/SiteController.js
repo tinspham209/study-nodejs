2f6e528887462553cde9f3d5b6f7e9db28461986
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-11 22:23:03 +0700

    Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,10 +1,16 @@
 const Course = require("../models/Course");
 
+const { multipleMongooseToObject } = require("../../util/mongoose");
+
 class SiteController {
 	// [GET] /
 	index(req, res, next) {
 		Course.find({})
-			.then((courses) => res.json(courses))
+			.then((courses) => {
+				res.render("home", {
+					courses: multipleMongooseToObject(courses),
+				});
+			})
 			.catch((error) => next(error));
 	}
 

a37ae4ef57b46635c1c255de833e8ad40e43367c
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-11 13:03:47 +0700

    Init /home & /search with MVC

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,9 @@
 const newsRouter = require("./news");
+const siteRouter = require("./site");
 
 function route(app) {
 	app.use("/news", newsRouter);
-
-	app.get("/", (req, res) => {
-		res.render("home");
-	});
-
-	app.get("/search", (req, res) => {
-		res.render("search");
-	});
-
-	app.post("/search", (req, res) => {
-		console.log("res.body", req.body);
-		res.send("");
-	});
+	app.use("/", siteRouter);
 }
 
 module.exports = route;
